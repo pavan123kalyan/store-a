21b5ba7acff748bb25fcc2fe10155faaa3a59d08
pavan123kalyan <136945801+pavan123kalyan@users.noreply.github.com>
2025-03-20 14:11:43 +0530

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,15 @@
-import "./Header.css"
-import {Link} from "react-router-dom";
-export default function Header(){
-    return(
-        
-        <div className="App-Header-Row">
-            <h1>My React store</h1>
-        <div>
-            <Link to="products">Home</Link>
-            <Link to="Login">Login</Link>
-        </div>
-        </div>
-    
-    );
+
+import "./Header.css";
+import { Link } from "react-router-dom";
+export default function Header() {
+  return (
+    <div className="App-Header-Row">
+      <h1>My React Store</h1>
+      <div className="App-Header-Links">
+        <Link to="products" className="App-Header-Link">Home</Link>
+        <Link to="cart" className="App-Header-Link">Cart</Link>
+        <Link to="login" className="App-Header-Link">Login</Link>
+      </div>
+    </div>
+  );
 }
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,17 +1,14 @@
-import React from 'react'
-export default function Login(){
-    return(
-        <form>
-            <label>Email: </label>
-            <input type="email" name="email" required/>
-            <br>
-            </br>
-            <label>Password: </label>
-            <input type="password" name="password" required/>
-            <br>
-            </br>
-            <button type="submit">Login</button>
-        </form>
-    )
 
+import React from 'react'
+import { Link } from 'react-router-dom'
+export default function Login() {
+  return (
+    <div>
+      <h3>Login Form</h3>
+      <p><input type="text" placeholder='Email address'></input></p>
+      <p><input type="password" placeholder='Password'></input></p>
+      <p><button>Log In</button></p>
+      <p><Link to="../register">Create Account</Link></p>
+    </div>
+  )
 }
