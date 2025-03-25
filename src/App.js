06faefee8dd41f885df4ecd26d5fbed70c40c517
pavan123kalyan <136945801+pavan123kalyan@users.noreply.github.com>
2025-03-25 12:08:58 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -4,20 +4,24 @@ import Header from "./components/Header";
 import Products from "./components/Products";
 import Footer from "./components/Footer";
 import Login from "./components/Login";
+import Register from "./components/Register"; // ✅ Import Register
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route index element={<Products />}></Route>
-          <Route path="products" element={<Products />}></Route>
-          <Route path="login" element={<Login />}></Route>
+          <Route index element={<Products />} />
+          <Route path="products" element={<Products />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} /> {/* ✅ Add this */}
         </Routes>
         <Footer />
       </BrowserRouter>
     </div>
   );
 }
+
 export default App;
