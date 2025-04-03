bd5e389feac517c4f9f45d39fcd0b2554a5ac329
pavan123kalyan <136945801+pavan123kalyan@users.noreply.github.com>
2025-04-03 09:29:40 +0530

    updated

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,22 +1,77 @@
-import React from 'react'
+// import React from 'react'
 
-import { appContext } from '../App';
-import { useContext } from 'react';
+// import { appContext } from '../App';
+// import { useContext } from 'react';
 
-export default function Cart(){
-    const {cart,setCart,products}=useContext(appContext);
-    const handleDelete=(id) =>{
-        setCart({...cart,[id]:0});
-    };
-    return(
-        //  <div>Cart</div>
-        <div>
+// export default function Cart(){
+//     const {cart,setCart,products}=useContext(appContext);
+//     const handleDelete=(id) =>{
+//         setCart({...cart,[id]:0});
+//     };
+//     const increment =(id) =>{
+//       setCart({...cart,[id]:0});
+//     };
+//     const decrement =(id) =>{
+//       if(cart[id]>1){
+//         setCart({...cart,[id]:cart[id]-1});
+//       }
+//       else{
+//         handleDelete(id);
+//       }
+//     }
+//     return(
+//         //  <div>Cart</div>
+//         <div>
+//       {products.map(
+//         (value) =>
+//           cart[value.id]>0 && (
+//             <div key={value.id}>
+//               {value.name}-{value.price}-<button>-</button>{cart[value.id]}<button>+</button>-
+//               {value.price * cart[value.id]}
+//               <button onClick={() => increment(value.id)}>-</button>
+//               {cart[value.id]}
+//               <button onClick={() => decrement(value.id)}>-</button>
+//               <button onClick={() => handleDelete(value.id)}>Delete</button>
+//             </div>
+//           )
+//       )}
+//     </div>
+//   );
+// }
+import React from "react";
+import { appContext } from "../App";
+import { useContext } from "react";
+
+export default function Cart() {
+  const { cart, setCart, products } = useContext(appContext);
+
+  const handleDelete = (id) => {
+    setCart({ ...cart, [id]: 0 });
+  };
+
+  const increment = (id) => {
+    setCart({ ...cart, [id]: (cart[id] || 0) + 1 });
+  };
+
+  const decrement = (id) => {
+    if (cart[id] > 1) {
+      setCart({ ...cart, [id]: cart[id] - 1 });
+    } else {
+      handleDelete(id); // Remove from cart when quantity is 0
+    }
+  };
+
+  return (
+    <div>
       {products.map(
         (value) =>
-          cart[value.id]>0 && (
+          cart[value.id] > 0 && (
             <div key={value.id}>
-              {value.name}-{value.price}-<button>-</button>{cart[value.id]}<button>+</button>-
-              {value.price * cart[value.id]}-
+              {value.name} - ₹{value.price} - {cart[value.id]} - ₹
+              {value.price * cart[value.id]}  
+              <button onClick={() => increment(value.id)}>+</button>  
+              {cart[value.id]}  
+              <button onClick={() => decrement(value.id)}>-</button>  
               <button onClick={() => handleDelete(value.id)}>Delete</button>
             </div>
           )
